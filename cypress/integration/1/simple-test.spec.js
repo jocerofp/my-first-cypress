5f15eecd93a872934f5a481d00f3289b0a96dd11
WPaczula <wojciech.paczula@gmail.com>
2021-02-26 21:07:55 +0100

    Add template for simple test

---
+++ cypress/integration/1/simple-test.spec.js
@@ -0,0 +1,25 @@
+describe('Greeting form', () => {
+    /*
+    Test case 1
+        Wejdź na stronę /1/simple-test
+        Wypełnij płeć - mężczyzna
+        Wpisz imię - Jan
+        Wyślij formularz przyciskiem "Wyślij"
+        Spodziewany rezultat: Zostaje wyświetlony toast z powitaniem: "Witaj Jan! Cieszę się, że wysłałeś ten formularz!"
+    */
+    it('should 1...', () => {
+        
+    })
+    
+    /*
+    Test case 2
+        Wejdź na stronę /1/simple-test
+        Wypełnij płeć - kobieta
+        Pozostaw imię puste
+        Wyślij formularz przyciskiem "Wyślij"
+        Spodziewany rezultat: Zostaje wyświetlony toast z powitaniem: "Cześć tajemnicza nieznajoma! Cieszę się że wysłałaś ten formularz!"
+    */
+   it('should 2...', () => {
+       
+   })
+})
