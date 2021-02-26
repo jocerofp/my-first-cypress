4576fa433a3384bce831694097809fd72f7bcdbb
WPaczula <wojciech.paczula@gmail.com>
2021-02-26 21:10:56 +0100

    Add template for second excercise

---
+++ cypress/integration/1/1.read-and-edit.spec.js
@@ -0,0 +1,29 @@
+describe('Get my emoji', () => {
+    /*
+    Test case 1
+        Wejdź na stronę /1/edit-save
+        Wczytaj liczbę emoji
+        Wejdź w tryb edycji za pomocą przycisku "Edytuj"
+        Zwiększ liczbę o 1 za pomocą strzałki w górę
+        Zaznacz checkbox, który sprawdza, że nie jesteś robotem
+        Zapisz zmiany za pomocą przycisku "Zapisz"
+        Spodziewany rezultat: Zostaje wyświetlony o jeden więcej emoji.
+
+    */
+    it('should 1...', () => {
+        
+    })
+    
+    /*
+    Test case 2
+        Wejdź na stronę /1/edit-save
+        Wczytaj liczbę emoji
+        Wejdź w tryb edycji za pomocą przycisku "Edytuj"
+        Wyczyść input "Liczba emoji"
+        Odrzuć zmiany za pomocą przycisku "Anuluj"
+        Spodziewany rezultat: Liczba emoji pozostaje bez zmian, ale one same się zmieniają
+    */
+   it('should 2...', () => {
+       
+   })
+})
