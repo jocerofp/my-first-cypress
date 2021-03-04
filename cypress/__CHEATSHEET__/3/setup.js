e7194faba2ef9548e78a3a99530dac72af86b63f
WPaczula <wojciech.paczula@gmail.com>
2021-03-04 19:37:53 +0100

    feat: Add cheatsheet for setup

---
+++ cypress/__CHEATSHEET__/3/setup.js
@@ -0,0 +1,13 @@
+// ================================== POSTAW TO SAM =========================================
+
+// Po pierwsze stwórz nowy folder i zainicjuj npma:
+`npm init`
+
+// Drugim krokiem będzie instalowanie cypressa
+`npm i cypress`
+
+// I odpalenie w trybie open, aby zainicjalizowac projekt
+`npx cypress open`
+
+// Kolejne razy mozna odpalać w trybie headless
+`npx cypress run`
