4c138be404429fd961c831e2cf55fb07c6364dfd
Wojciech Paczuła <wojciech.paczula@gmail.com>
2021-04-26 09:07:59 +0200

    Fix typo

--- cypress/integration/2.debugging-tests/2.simulating-requests.spec.js
+++ cypress/integration/2.debugging-tests/2.simulating-requests.spec.js
@@ -62,15 +62,15 @@ describe("Simulating requests", () => {
   });
 
   it("should show information if blik succeeded", () => {
-    cy.intercept("POST", "/api/blik").as("lackOfFunds");
+    cy.intercept("POST", "/api/blik").as("request");
     const amount = 5000;
     const phone = "000-000-000";
 
     simulatingRequestsPage.ammountInput().type(amount);
     simulatingRequestsPage.phoneInput().type(phone);
     simulatingRequestsPage.sendButton().click();
-    simulatingRequestsSelectors.sendButton().click();
-    cy.wait("@lackOfFunds");
+    simulatingRequestsPage.sendButton().click();
+    cy.wait("@request");
 
     cy.contains(
       `Kwota ${amount}PLN została poprawnie przelana na numer ${phone}`
