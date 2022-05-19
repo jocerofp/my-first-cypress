24dc55415a4683f731d7a7a867d2469d36d5927b
jocerofp <jocero@future-processing.com>
2022-05-19 17:29:14 +0800

    Feat: add more API tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,17 +1,16 @@
-// 🐼 Add your log in command
+import REQUEST from './lib/api/request';
+/**
+ * new
+ */
+ Cypress.Commands.add('req', (params) => {
 
-Cypress.Commands.add('YOUR_COMMAND_NAME', () => {
-// 🐼 Fill the inputs and click log in
-// the login mechanism will automatically redirect you
-// back to the page you wanted to visit
-})
+	params.headers = {
+		'Content-Type': 'application/json',
+    }
 
-// The name should be "login" - all methods start with a small letter in cypress
-// thte body is a copy paste from the login part in your test /integration/login.spec.js
+	return new REQUEST(Cypress.env('url'), params).triggered();
+});
 
-/*
-* check all labels should be correct and intact
-*/
 
 Cypress.Commands.add('verifyTextLabels', (textData = []) => {
     textData.forEach(function (data) {
@@ -34,4 +33,170 @@ Cypress.Commands.add('loginPage', (username, password) => {
     cy.get("span[class='element']").eq(1).click(); // chck to check the checkbox for agree to storage...
     cy.get("button[name='login']").click()
     
+})
+/*
+* GEnerate access token from Azure AD 
+*/
+Cypress.Commands.add("loginAD", (overrides = {}) => {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+     <s:Header>
+        <a:Action s:mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</a:Action>
+        <a:ReplyTo>
+           <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+        </a:ReplyTo>
+        <a:To s:mustUnderstand="1">https://login.microsoftonline.com/extSTS.srf</a:To>
+        <o:Security xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" s:mustUnderstand="1">
+           <o:UsernameToken>
+              <o:Username>${Cypress.env('username')}</o:Username>
+              <o:Password>${Cypress.env('password')}</o:Password>
+           </o:UsernameToken>
+        </o:Security>
+     </s:Header>
+     <s:Body>
+        <t:RequestSecurityToken xmlns:t="http://schemas.xmlsoap.org/ws/2005/02/trust">
+           <wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy">
+              <a:EndpointReference>
+                 <a:Address>https://${Cypress.env('tenantId')}.sharepoint.com/</a:Address>
+              </a:EndpointReference>
+           </wsp:AppliesTo>
+           <t:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</t:KeyType>
+           <t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType>
+           <t:TokenType>urn:oasis:names:tc:SAML:1.0:assertion</t:TokenType>
+        </t:RequestSecurityToken>
+     </s:Body>
+  </s:Envelope>`
+
+const headers = { Authorization: "" };
+const xmlHeaders = {} // { 'Content-Type', 'application/xml'}
+const cookieHeaders = {
+  Host: `${Cypress.env('tenantId')}.sharepoint.com`
+}
+cy.request({
+  method: "POST",
+  url: `https://login.microsoftonline.com/d70890a4-3ec5-4da3-b812-61e6b3ff4ae7/oauth2/token`,
+  header: {
+    "cache-control": "no-cache",
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  form: true,
+  body: {
+    // grant_type: "client_credentials",
+    client_id: Cypress.env('clientId'),
+    grant_type: "client_credentials",
+    // client_secret: "x6gbHZt3BEDkeVYRPxHZkfVVWRGUuxwwGynmeUJ14wk=",
+    // scope: "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d%2F.default",
+    // resource: "https%3A%2F%2Fmanagement.core.windows.net%2F",
+    scope: 'https://graph.microsoft.com/.default',
+    client_secret: Cypress.env('client_secret'),
+    },
+  }).then(response => {
+      const ADALToken = response.body.access_token;
+      const expiresOn = response.body.expires_on;
+      // console.log(ADALToken)
+    //   localStorage.setItem("adal.token.keys", `${Cypress.config("clientId")}|`);
+    //   localStorage.setItem(`adal.access.token.key${Cypress.config("clientId")}`, ADALToken);
+    //   localStorage.setItem(`adal.expiration.key${Cypress.config("clientId")}`, expiresOn);
+    //   localStorage.setItem("access_token", ADALToken);
+      // window.sessionStorage.setItem("expiration_key", expiresOn);
+      // window.sessionStorage.setItem("access_token", ADALToken);
+      // localStorage.setItem("expiration_key", expiresOn);
+      // localStorage.setItem("access_token", ADALToken);
+      headers.Authorization = `Bearer ${ADALToken}`;
+    
+      console.log(xml);
+      cy.request({
+        method: 'POST',
+        url: 'https://login.microsoftonline.com/extSTS.srf',
+        headers: xmlHeaders,
+        form: false,
+        body: xml,
+      }).then(xmlresponse => {
+        // console.log(xmlresponse.body)
+        var oParser = new DOMParser();
+        var oDOM = oParser.parseFromString(xmlresponse.body, "application/xml");
+        cy.log(oDOM);
+        var tokens = oDOM.getElementsByTagName('wsse:BinarySecurityToken');
+        // console.log(tokens);
+        // const magicString = tokens[0].innerHTML;
+      //   cy.log(magicString);
+      //   cy.request({
+      //       method: 'POST',
+      //       url: `https://${Cypress.env('tenantId')}.sharepoint.com/_forms/default.aspx?wa=wsignin1.0`,
+      //       headers: cookieHeaders,
+      //       form: false,
+      //       body: magicString,
+      //     }).then(cookieResponse => {
+      //       cy.log(cookieResponse)
+      // });
+    });
+  })
+})
+
+  /*
+* GEnerate access token from Azure AD 
+*/
+Cypress.Commands.add("authenticateUsingToken", () => {
+    cy.request({
+      method: "POST",
+      url: `https://login.microsoftonline.com/d70890a4-3ec5-4da3-b812-61e6b3ff4ae7/oauth2/token`,
+      header: {
+        "cache-control": "no-cache",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      form: true,
+      body: {
+        grant_type: "client_credentials",
+        username: "jocero@future-processing.com",
+        password: "T3st2022@#$",
+        client_id: "fd10d2bc-91a1-4115-b8c2-409d4eb42334",
+        client_secret: "x6gbHZt3BEDkeVYRPxHZkfVVWRGUuxwwGynmeUJ14wk=",
+        scope: "api://TeamsAllocationManagerApi/Users.Read"
+      },
+    }).then(response => {
+      const ADALToken = response.body.access_token;
+      const expiresOn = response.body.expires_on;
+      console.log(ADALToken)
+      window.sessionStorage.setItem("expiration_key", expiresOn);
+      window.sessionStorage.setItem("access_token", ADALToken);
+      window.sessionStorage.setItem("X-ZUMO-AUTH", Cypress.env('X-ZUMO-AUTH'));
+      window.sessionStorage.setItem("X-MS-TOKEN-AAD-ACCESS-TOKEN", Cypress.env('X-MS-TOKEN-AAD-ACCESS-TOKEN'));
+    });
+  })
+
+
+
+  /*
+* GEnerate access token from Azure AD 
+*/
+Cypress.Commands.add("loginAD2", () => {
+ 
+cy.request({
+  method: "POST",
+  url: `https://login.microsoftonline.com/${Cypress.env('tenantId')}/oauth2/token`,
+  header: {
+    "cache-control": "no-cache",
+    "Content-Type": "application/x-www-form-urlencoded"
+  },
+  form: true,
+  body: {
+    grant_type: 'password',
+    client_id: Cypress.env('clientId'),
+    client_secret: Cypress.env('client_secret'),
+    resource: "https%3A%2F%2Fteamsallocationmanager.z16.web.core.windows.net%2F",
+    username: Cypress.env('username'),
+    password: Cypress.env('password')
+    },
+  }).then(response => {
+      const accessToken = response.body.access_token;
+      const expiresOn = response.body.expires_on;
+      cy.window().then((crntWindow) => {
+        crntWindow.sessionStorage.setItem(`adal.token.keys`, `${config.resource}|`);
+        crntWindow.sessionStorage.setItem(`adal.expiration.key${config.resource}`, expires);
+        crntWindow.sessionStorage.setItem(`adal.access.token.key${config.resource}`, accessToken);
+        crntWindow.sessionStorage.setItem('X-ZUMO-AUTH', Cypress.env('X-ZUMO-AUTH'));
+        crntWindow.sessionStorage.setItem('X-MS-TOKEN-AAD-ACCESS-TOKEN', Cypress.env('X-MS-TOKEN-AAD-ACCESS-TOKEN'));
+        // cy.visit(Cypress.env('tamURL'))
+    });
+  })
 })
